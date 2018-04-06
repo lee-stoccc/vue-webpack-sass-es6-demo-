8b0edf22b9f511bc40dd16ef810fcc24f62b42c4
lee <870722422@qq.com>
2018-04-06 10:55:40 +0800

    增加模块

---
+++ src/components/Center/Cen.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="parent">
+        <div class="child">sfsdfsss</div>
+    </div>
+</template>
+
+<style src="./center.sccc" lang="scss" scoped="">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/hello/HelloWorld'
+import Center from '@/components/Center/Cen.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+      {
+          path:'/center',
+          name:'Center',
+          component:Center
+      }
   ]
 })
